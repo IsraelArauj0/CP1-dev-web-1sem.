94ca8b0be620691468a4b8c4979dec1126083a6c
Israel <israel11.araujo@gmail.com>
2024-04-01 19:20:24 -0300

    Enviando a primeira questão //Israel ;; https://github.com/IsraelArauj0

---
+++ script.js
@@ -0,0 +1,5 @@
+//Primeira questão
+let indef; // Cria uma variável indefinida
+let vazia = null; // Cria uma variável vazia
+let valor = "este é um exemplo" // Cria uma variável com valor
+document.write(vazia)
