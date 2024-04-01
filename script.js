64470c59384ef9a256238a1d6cf29c85c24cbce8
Israel <israel11.araujo@gmail.com>
2024-04-01 20:31:53 -0300

    Commit da quarta questão //Israel //  https://github.com/IsraelArauj0

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ else{
 }
 
 //terceira questão
-let peso = 100; // Cria uma variável com valor 100kg
+/*let peso = 100; // Cria uma variável com valor 100kg
 let altura = 1.8; // Cria uma variável com valor 1.8 metros
 let imc = peso / (altura * altura) // faz o calculo da formula do IMC
 if(imc > 24.9){ // verifica se o imc é maior do que 24,4
@@ -43,4 +43,17 @@ else{
         console.log("abaixo do peso")
     }
 }
-console.log("O seu IMC é: ", imc)
+console.log("O seu IMC é: ", imc) */
+
+//quarto exercicio
+let idade =10 // define uma idade para a pessoa, não consegui puxar a idade, estava dando erro no document
+if (idade <= 12) { // se a idade da pessoa for menor ou igual a 12 é considerada criança
+    console.log("Criança") ;
+} else if (idade <= 18) { // se a idade da pessoa for menor ou igual a 18 é considerada Adolescente
+    console.log("Adolescente");
+} else if (idade <= 60) { // se a idade da pessoa for menor ou igual a 60 é considerada adulta
+    console.log("Adulto");
+} else {
+    console.log("Idoso");
+}
+
