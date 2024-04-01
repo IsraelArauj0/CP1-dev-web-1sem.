a761848adda40a4b558d0ebe01405443f7dcf2f4
Israel <israel11.araujo@gmail.com>
2024-04-01 19:46:19 -0300

    Commit da segunda questão //Israel //  https://github.com/IsraelArauj0

--- script.js
+++ script.js
@@ -2,4 +2,27 @@
 let indef; // Cria uma variável indefinida
 let vazia = null; // Cria uma variável vazia
 let valor = "este é um exemplo" // Cria uma variável com valor
-document.write(vazia)
+//console.log(valor, vazia, indef) 
+//Segunda questão
+let num = 10; // Cria uma variável com valor 10
+let num1 = 11; // Cria uma variável com valor 11
+if(num1 != num){ //verifica se o valor da variável num é diferente da variável num1
+    console.log(num)
+}
+else{
+  console.log(num1)
+}
+
+if(num1 == num){ //verifica se o valor da variável num1 é igual ao da variável num
+    console.log(num1)
+}
+else{
+  console.log(num)
+}
+
+if(num1 >= num){ //verifica se o valor da variável num1 é maior ou igual ao da variável num
+    console.log(num1)
+}
+else{
+  console.log(num)
+}
